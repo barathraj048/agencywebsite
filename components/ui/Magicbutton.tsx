934b0a91a62dbd47ce86f31019ed1b22446f540d
Barath <barathraj048@gmail.com>
2024-12-02 16:15:27 +0530

    fix styling

--- components/ui/Magicbutton.tsx
+++ components/ui/Magicbutton.tsx
@@ -20,7 +20,7 @@ const Magicbutton: React.FC = () => {
       <span className="transition ease-in-out delay-150 absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
       <span
         className="transition ease-in-out delay-150 duration-500 hover:bg-gradient-custom2 inline-flex h-full w-full cursor-pointer items-center justify-center bg-slate-950 text-sm font-medium text-white text-opacity-70 backdrop-blur-3xl bg-gradient-custom px-8 py-4 rounded-xl gap-4
-      md:px-8 md:py-4 px-6 py-3 text-sm md:text-base"
+      md:px-8 md:py-4  md:text-base"
       >
         See More About Us{" "}
         <ArrowForwardIcon
